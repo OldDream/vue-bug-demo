5cc96e5a8066a8827fad4b7ddb03fe98d3e6c7ce
Old_dream <huangyn15@qq.com>
2020-08-31 12:59:06 +0800

    add: Solution

--- src/components/MainPage/MainPage.vue
+++ src/components/MainPage/MainPage.vue
@@ -4,24 +4,23 @@
     <!-- bug shows up in the dom below, if we add v-once or give it a key, the bug disappear. 
       So I think there might be a bug during diff and patch.
     -->
-    <div id="aMapContainer"></div>
+    <Test />
     <div class="navWrapper" v-show="!isAutoNavStarted">
-      <nav-bar
-        
-        :arrayData="navBarData"
-        ref="navBar"
-      ></nav-bar>
+      <nav-bar :arrayData="navBarData" ref="navBar"></nav-bar>
     </div>
-    <div class="btn" v-if="!isAutoNavStarted" @click="toggle">Click Me First</div>
+    <div class="btn" v-if="!isAutoNavStarted" @click="toggle">
+      Click Me First
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainPage',
+  name: "MainPage",
   components: {
-    ExitAutoNaviBtn: () => import('./component/ExitAutoNaviBtn'),
-    NavBar: () => import('./component/NavBar'),
+    ExitAutoNaviBtn: () => import("./component/ExitAutoNaviBtn"),
+    NavBar: () => import("./component/NavBar"),
+    Test: () => import("./component/Test"),
   },
   data() {
     return {
@@ -29,25 +28,22 @@ export default {
       navBarData: [
         {
           id: -3,
-          name: '1',
-          img: require('./imgs/navBar/scenicArea.png'),
-          grayImg:
-            ' ',
+          name: "1",
+          img: require("./imgs/navBar/scenicArea.png"),
+          grayImg: " ",
         },
         {
           id: -2,
-          name: '2',
-          img: require('./imgs/navBar/scenicList.png'),
-          grayImg:
-            ' ',
+          name: "2",
+          img: require("./imgs/navBar/scenicList.png"),
+          grayImg: " ",
           iconCode: 1,
         },
         {
           id: null,
-          name: '3',
-          img: require('./imgs/navBar/scenicMark.png'),
-          grayImg:
-            ' ',
+          name: "3",
+          img: require("./imgs/navBar/scenicMark.png"),
+          grayImg: " ",
           iconCode: 1,
         },
       ],
@@ -55,16 +51,11 @@ export default {
   },
   methods: {
     toggle() {
-      this.isAutoNavStarted = !this.isAutoNavStarted
+      this.isAutoNavStarted = !this.isAutoNavStarted;
     },
-    addDom() {
-      let node1 = document.querySelector('#aMapContainer')
-      node1.innerHTML = `<div class="dynamicAdded">this DOM is added by  Vanilla JS after mounted</div>`
-    }
   },
 
   async mounted() {
-    this.addDom()
   },
 };
 </script>
---
+++ src/components/MainPage/component/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="aMapContainer"></div>
+</template>
+
+<script>
+export default {
+  name: "TEST",
+  data() {
+    return {};
+  },
+  methods: {
+    addDom() {
+      let node1 = document.querySelector("#aMapContainer");
+      node1.innerHTML = `<div class="dynamicAdded">this DOM is added by  Vanilla JS after mounted</div>`;
+    },
+  },
+
+  mounted() {
+    this.addDom();
+  },
+};
+</script>
+
+<style lang="scss">
+.dynamicAdded {
+  height: 100%;
+  background-color: rgb(196, 250, 0);
+  padding-top: 100px;
+}
+</style>
